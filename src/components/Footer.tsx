29e0487d43dd70ea85562a352e5e4bbaf94c5de8
imaty1022 <vo2.m1lxxx@gmail.com>
2025-06-15 23:07:27 +0900

    del: 不要なモジュールを削除

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-10 py-6 text-center text-sm text-gray-500 border-t">
-      &copy; {new Date().getFullYear()} 今井 大祥 Portfolio. All rights reserved.
-    </footer>
-  );
-}
