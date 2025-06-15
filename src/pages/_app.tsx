83551b79a1d9315bc2f6e766fb9c67885bdf749a
imaty1022 <vo2.m1lxxx@gmail.com>
2025-06-15 23:27:50 +0900

    update: pfp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>imaty | ポートフォリオ</title>
-        <meta name="description" content="imatyの職務経歴・スキル・プロジェクトを紹介するシンプルなポートフォリオサイトです。" />
+        <title>imaty | Portfolio</title>
+        <meta name="description" content="imaty's portfolio" />
+        <link rel="icon" href="/profile/profile.png" />
       </Head>
       <main className="min-h-screen py-10 px-4 max-w-2xl mx-auto">
         <Component {...pageProps} />
