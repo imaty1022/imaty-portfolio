e29d5dfa9d0d26944f8cd1af9a75bb1b5e3d3551
imaty1022 <vo2.m1lxxx@gmail.com>
2025-06-22 22:47:29 +0900

    feat: add background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,21 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>imaty | Portfolio</title>
         <meta name="description" content="imaty's portfolio" />
-        <link rel="icon" href="/profile/profile.png" />
+        <link rel="icon" href="/images/profile/profile.png" />
       </Head>
-      <main className="min-h-screen py-10 px-4 max-w-2xl mx-auto">
-        <Component {...pageProps} />
-      </main>
+      <div className="relative min-h-screen">
+        <div 
+          className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat opacity-40" 
+          style={{ 
+            backgroundImage: 'url(/images/background.png)',
+            backgroundAttachment: 'fixed'
+          }}
+        />
+        <div className="absolute inset-0 -z-20 bg-gradient-to-b from-white/70 to-white/80 dark:from-gray-900/80 dark:to-gray-800/80" />
+        <main className="relative py-10 px-4 max-w-2xl mx-auto z-10">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </>
   );
 }
